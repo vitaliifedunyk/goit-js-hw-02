c8d61ecdaf3b9bca05ac62384b9646f85d13c87d
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2025-11-13 23:08:27 +0100

    add 4 task

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+function checkForSpam(message) {
+  message = message.toLowerCase();
+  //   let check1 = message.includes('spam');
+  //   let check2 = message.includes('sale');
+  let result = message.includes('spam') || message.includes('sale');
+
+  if (result) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(checkForSpam('Latest technology news')); // false
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+// console.log(checkForSpam('Trust me, this is not a spam message')); // true
+// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
